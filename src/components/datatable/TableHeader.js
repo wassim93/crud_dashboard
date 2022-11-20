32c83edf4e519ae88d34532cbe22806ad6e89b5a
wassim93 <wassim.benhassen@esprit.tn>
2022-11-20 15:31:41 +0100

    fix

--- src/components/datatable/TableHeader.js
+++ src/components/datatable/TableHeader.js
@@ -1,6 +1,5 @@
 import React from "react";
 import PropTypes from "prop-types";
-
 import { Checkbox, TableRow, TableCell, TableHead, TableSortLabel } from "@mui/material";
 
 function TableHeader({ cells, hasCheckbox, hasActions, sortDirection, sortAction, sortBy, onSelectAllClick }) {
@@ -33,10 +32,6 @@ TableHeader.propTypes = {
   sortAction: PropTypes.func,
   sortDirection: PropTypes.oneOf(["asc", "desc"]),
   sortBy: PropTypes.string,
-  // rowCount: PropTypes.number,
-  // headLabel: PropTypes.array,
-  // numSelected: PropTypes.number,
-  // onRequestSort: PropTypes.func,
   onSelectAllClick: PropTypes.func,
 };
 
